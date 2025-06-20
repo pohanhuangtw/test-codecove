60caaee9cbbff102c43023dbd47c1cfe8c0014f9
pohanhuangtw <pohan.huang@suse.com>
2025-06-20 11:17:59 +0800

    new commit
    Signed-off-by: pohanhuangtw <pohan.huang@suse.com>

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mul(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
+}
+
+func main() {
+	fmt.Println(add(1, 2))
+	fmt.Println(sub(1, 2))
+	fmt.Println(mul(1, 2))
+	fmt.Println(div(1, 2))
+}
